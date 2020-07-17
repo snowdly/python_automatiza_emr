fb85195014fa6a5df6591f9d2dd11c5b59e9f2d2
bsansol <68106691+bsansol@users.noreply.github.com>
2020-07-17 12:34:47 +0200

    Add files via upload

--- validacion_web/infoantenas.py
+++ validacion_web/infoantenas.py
@@ -4,6 +4,9 @@ import re
 import json
 import requests
 import os
+import sys
+sys.path.insert(0, '../')
+from principal import procesos_comunes
 
 
 """
@@ -43,41 +46,8 @@ print(Longitud)
 
 def obtiene_datos(Longitud,Latitud,ficheros_respaldo):
 
-	#convertivos coordenadas en grados a coordenadas en decimal
-	signo=-1
-	#sustituir tanto en ingles como en español
-	if Latitud.find('N') > 0:
-		Latitud= Latitud.replace('N', '-')
-
-	else:
-		Latitud= Latitud.replace('S', '-')
-
-	Latitud= Latitud.replace(' ', '-')#para xml hay que poner el guion despues de dos digitos de minutos, en vez de sustituir espacio
-	Latitud= Latitud.replace(',', '.')
-
-
-	if Longitud.find('W') > 0:
-		Longitud= Longitud.replace('W', '-')
-
-	elif Longitud.find('O') > 0:
-		Longitud= Longitud.replace('O', '-')
-
-	elif Longitud.find('E') > 0:
-		Longitud= Longitud.replace('E', '-')
-		signo=1
-
-
-	Longitud= Longitud.replace(' ', '-')#para xml hay que poner el quion despues de dos digitos de minutos, en vez de sustituir espacio
-	Longitud= Longitud.replace(',', '.')
-
-
-	deg, minutes, seconds =  re.split('-', Latitud)
-	CoordenadaY= float(deg) + float(minutes)/60 + float(seconds)/(60*60)
-
-	deg, minutes, seconds =  re.split('-', Longitud)
-	CoordenadaX=(float(deg) + float(minutes)/60 + float(seconds)/(60*60))* signo
-
-
+	#convertivos coordenadas en grados a coordenadas en decimal ETRS89
+	CoordenadaX,CoordenadaY=procesos_comunes.coordenadas_ETRS89(Longitud,Latitud)
 
 	#calculamos los puntos de la diagonal
 	#factor 0.0005 equivale a 5 segundos, la diagonal sera de 10 segundos en total
@@ -109,4 +79,4 @@ def obtiene_datos(Longitud,Latitud,ficheros_respaldo):
 
 #infoantenas('04W54 53,63','37N51 01,13','{"type":"FeatureCollection","features":[{"type":"Feature","geometry":{"type":"Point","coordinates":[-4.914917,37.850277]},"properties":{"Gis_Latitud":"37.850278","Gis_Longitud":"-4.914917","Gis_ID":"001736","Gis_Etiqueta":"Estación de telefonía móvil","Gis_Estilo":"vcne.estaciones","Gis_Codigo":"VODAFONE ESPAÑA, S.A. - 001736","Tipo":"Estación de telefonía móvil","Código":"VODAFONE ESPAÑA, S.A. - 001736","Dirección":"CL carretera Palma del rio A-431 Km 11,5, s\/n. CÓRDOBA, CÓRDOBA","Detalle":"@@<url-aplicacion>\/detalleEstacion.do?emplazamiento=001736"},"id":"001736"},{"type":"Feature","geometry":{"type":"Point","coordinates":[-4.912792,37.850864]},"properties":{"Gis_Latitud":"37.850864","Gis_Longitud":"-4.912792","Gis_ID":"01736","Gis_Etiqueta":"Estación de telefonía móvil","Gis_Estilo":"vcne.estaciones","Gis_Codigo":"VODAFONE ESPAÑA, S.A. - 01736","Tipo":"Estación de telefonía móvil","Código":"VODAFONE ESPAÑA, S.A. - 01736","Dirección":"CR A-431 CO-PALMA DEL RIO (VILLARRUBIA), 12. CÓRDOBA, CÓRDOBA","Detalle":"@@<url-aplicacion>\/detalleEstacion.do?emplazamiento=01736"},"id":"01736"},{"type":"Feature","geometry":{"type":"Point","coordinates":[-4.9149,37.850323]},"properties":{"Gis_Latitud":"37.850322","Gis_Longitud":"-4.914900","Gis_ID":"1400453","Gis_Etiqueta":"Estación de telefonía móvil","Gis_Estilo":"vcne.estaciones","Gis_Codigo":"TELEFONICA MOVILES ESPAÑA, S.A.U. - 1400453","Tipo":"Estación de telefonía móvil","Código":"TELEFONICA MOVILES ESPAÑA, S.A.U. - 1400453","Dirección":"VP Villarrubia, S\/N. CÓRDOBA, CÓRDOBA","Detalle":"@@<url-aplicacion>\/detalleEstacion.do?emplazamiento=1400453"},"id":"1400453"},{"type":"Feature","geometry":{"type":"Point","coordinates":[-4.9149,37.850323]},"properties":{"Gis_Latitud":"37.850322","Gis_Longitud":"-4.914900","Gis_ID":"1736","Gis_Etiqueta":"Estación de telefonía móvil","Gis_Estilo":"vcne.estaciones","Gis_Codigo":"VODAFONE ESPAÑA, S.A. - 1736","Tipo":"Estación de telefonía móvil","Código":"VODAFONE ESPAÑA, S.A. - 1736","Dirección":"CR PALMA DEL RIO, 12. CÓRDOBA, CÓRDOBA","Detalle":"@@<url-aplicacion>\/detalleEstacion.do?emplazamiento=1736"},"id":"1736"},{"type":"Feature","geometry":{"type":"Point","coordinates":[-4.9149,37.850323]},"properties":{"Gis_Latitud":"37.850322","Gis_Longitud":"-4.914900","Gis_ID":"1B5CO2738","Gis_Etiqueta":"Estación de telefonía móvil","Gis_Estilo":"vcne.estaciones","Gis_Codigo":"XFERA MOVILES, S.A. - 1B5CO2738","Tipo":"Estación de telefonía móvil","Código":"XFERA MOVILES, S.A. - 1B5CO2738","Dirección":"CR PALMA DEL RIO, 12(K). CÓRDOBA, CÓRDOBA","Detalle":"@@<url-aplicacion>\/detalleEstacion.do?emplazamiento=1B5CO2738"},"id":"1B5CO2738"},{"type":"Feature","geometry":{"type":"Point","coordinates":[-4.9235,37.843552]},"properties":{"Gis_Latitud":"37.843553","Gis_Longitud":"-4.923500","Gis_ID":"ANDR6803B","Gis_Etiqueta":"Estación de telefonía móvil","Gis_Estilo":"vcne.estaciones","Gis_Codigo":"ORANGE ESPAGNE, SAU - ANDR6803B","Tipo":"Estación de telefonía móvil","Código":"ORANGE ESPAGNE, SAU - ANDR6803B","Dirección":"CL H PP-V.1 222(M) Suelo PP V1 M222 VPO, S\/N. CÓRDOBA, CÓRDOBA","Detalle":"@@<url-aplicacion>\/detalleEstacion.do?emplazamiento=ANDR6803B"},"id":"ANDR6803B"}]}',)
 
-#infoantenas('02E08 51,63','41N22 29,75','{"type":"Feature","geometry":{"type":"Point","coordinates":[2.144789,41.375168]},"properties":{"Gis_Latitud":"41.375167","Gis_Longitud":"2.144789","Gis_ID":"0801040","Gis_Etiqueta":"Estación de telefonía móvil","Gis_Estilo":"vcne.estaciones","Gis_Codigo":"TELEFONICA MOVILES ESPAÑA, S.A.U. - 0801040","Tipo":"Estación de telefonía móvil","Código":"TELEFONICA MOVILES ESPAÑA, S.A.U. - 0801040","Dirección":"CL Creu Coberta, 83. BARCELONA, BARCELONA","Detalle":"@@<url-aplicacion>\/detalleEstacion.do?emplazamiento=0801040"},"id":"0801040"')
+#obtiene_datos('02E0851,63','41N2229,75','{"type":"Feature","geometry":{"type":"Point","coordinates":[2.144789,41.375168]},"properties":{"Gis_Latitud":"41.375167","Gis_Longitud":"2.144789","Gis_ID":"0801040","Gis_Etiqueta":"Estación de telefonía móvil","Gis_Estilo":"vcne.estaciones","Gis_Codigo":"TELEFONICA MOVILES ESPAÑA, S.A.U. - 0801040","Tipo":"Estación de telefonía móvil","Código":"TELEFONICA MOVILES ESPAÑA, S.A.U. - 0801040","Dirección":"CL Creu Coberta, 83. BARCELONA, BARCELONA","Detalle":"@@<url-aplicacion>\/detalleEstacion.do?emplazamiento=0801040"},"id":"0801040"')

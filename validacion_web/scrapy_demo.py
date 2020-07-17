c6af339bc74f7f03a4f3fe19ad7c2fc3aca247f2
Oscar Campos <snowdly@gmail.com>
2020-07-17 11:20:57 +0200

    Commit primera version

--- validacion_web/scrapy_demo.py
+++ validacion_web/scrapy_demo.py
@@ -1,1 +1,10 @@
-print("demo: ")
+import requests
+
+response = requests.get('https://geoportal.minetur.gob.es/VCTEL/infoantenasGeoJSON.do?bbox=-3.7048325892838%2C40'
+                        '.433671436071%2C-3.7014061138918%2C40.435990772009&zoom=3')
+#print(response.json())
+
+for feature in response.json()['features']:
+    print(feature)
+
+
